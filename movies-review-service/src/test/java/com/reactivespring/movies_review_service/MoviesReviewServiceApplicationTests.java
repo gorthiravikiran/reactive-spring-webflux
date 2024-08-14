937c936d0d16f9224b25d57c102ae032e5151cf6
Ravi Kiran Gorthi <Ravi.Gorthi@albertsons.com>
2024-08-14 16:07:54 +0530

    first commit

---
+++ movies-review-service/src/test/java/com/reactivespring/movies_review_service/MoviesReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactivespring.movies_review_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
